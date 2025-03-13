1d0a63fbfbfccf0f30fed0076b18fbca207dcd13
JakeH <Jake.heyser@live.com>
2025-03-12 23:54:28 -0400

    Adding routes for the header tabs and features to home page

--- day-83-personal-website/main.py
+++ day-83-personal-website/main.py
@@ -10,5 +10,25 @@ def home():
     current_year = datetime.now().year
     return render_template("index.html", current_year=current_year)
 
+@app.route("/athletics")
+def athletics():
+    current_year = datetime.now().year
+    return render_template("athletics.html", current_year=current_year)
+
+@app.route("/academics")
+def academics():
+    current_year = datetime.now().year
+    return render_template("academics.html", current_year=current_year)
+
+@app.route("/professional")
+def professional():
+    current_year = datetime.now().year
+    return render_template("professional.html", current_year=current_year)
+
+@app.route("/contact")
+def contact():
+    current_year = datetime.now().year
+    return render_template("contact.html", current_year=current_year)
+
 if __name__ == "__main__":
     app.run(debug=True)
